db32d66807711627e653ad932b911d1caa575b3f
YaccarinoMarit <137515083+YaccarinoMarit@users.noreply.github.com>
2023-06-25 21:13:33 +0700

    Fix a bug

---
+++ island_dog_elephant.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	
+	// Introduce program
+	fmt.Println("Welcome to the Self-Care Workshop!")
+	fmt.Println("Here, we will explore ways to practice self-care and show how it can benefit us in our daily lives.")
+	time.Sleep(2 * time.Second)
+	
+	// Talk about the power of self-care
+	fmt.Println("Self-care is the practice of taking an active role in protecting one's own wellbeing and happiness.")
+	fmt.Println("Having a strong self-care practice can help us to avoid stress, regulate emotions, and prevent burnout.")
+	time.Sleep(2 * time.Second)
+	
+	// Talk about the benefits of self-care
+	fmt.Println("By practicing self-care consistently, we can:")
+	fmt.Println("* Feel better about ourselves and our accomplishments")
+	fmt.Println("* Reduce anxiety and depression symptoms")
+	fmt.Println("* Have more energy and physical strength")
+	fmt.Println("* Improve our mental clarity and focus")
+	fmt.Println("* Increase our motivation and productivity")
+	fmt.Println("* Build stronger relationships with the people around us")
+	fmt.Println("* Enhance our overall wellbeing")
+	time.Sleep(2 * time.Second)
+	
+	// Talk about the types of self-care
+	fmt.Println("When we think of self-care, we often think of physical activities like yoga, meditation, or even getting a massage.")
+	fmt.Println("But self-care can also include other types of activities, such as:")
+	fmt.Println("* Eating healthy and nutritious foods")
+	fmt.Println("* Taking time for yourself and doing something you enjoy")
+	fmt.Println("* Getting enough rest and sleep")
+	fmt.Println("* Spending time with friends and family")
+	fmt.Println("* Giving yourself permission to say 'no'")
+	time.Sleep(2 * time.Second)
+	
+	// Suggest some self-care ideas
+	fmt.Println("Here are some ideas for how to practice self-care today:")
+	fmt.Println("* Take a walk in the park and notice the sights and sounds around you")
+	fmt.Println("* Cook a nutritious dinner for yourself and savor the flavors")
+	fmt.Println("* Do a gentle yoga practice, focusing on feeling your breath")
+	fmt.Println("* Write in a journal to explore your thoughts and feelings")
+	fmt.Println("* Listen to your favorite music and let it uplift your mood")
+	fmt.Println("* Call a friend or family member and check in with them")
+	time.Sleep(2 * time.Second)
+	
+	// Closing
+	fmt.Println("We hope that this workshop has given you some insight into how to practice self-care in your life.")
+	fmt.Println("Remember to take some time for yourself each day and appreciate the moments of joy and happiness that come your way!")
+	fmt.Println("Thanks for joining us, and have a great day!")
+}
